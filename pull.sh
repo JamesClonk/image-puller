232d849200d04dc2160cf3cc904e2967532ddea6
JamesClonk <jamesclonk@jamesclonk.ch>
2020-09-02 20:57:16 +0200

    support sha256

--- pull.sh
+++ pull.sh
@@ -2,6 +2,18 @@
 
 for image in $(cat /app/images.dat); do
 	echo "pulling ${image} ..."
-	crane ls $(echo "${image}" | sed 's/:.*//g')
-	crane pull ${image} tmp.tgz && rm -f tmp.tgz
+	if [[ $(echo "${image}" | grep sha256) ]]; then
+		image_base=$(echo "${image}" | sed 's/\(.*\)@sha256:.*/\1/g')
+		crane ls ${image_base}
+		images=$(crane ls ${image_base})
+		for image in $images; do
+			echo "${image_base}:${image}"
+			crane pull "${image_base}:${image}" tmp.tgz && rm -f tmp.tgz
+		done
+	else
+		crane ls $(echo "${image}" | sed 's/:.*//g')
+		crane pull ${image} tmp.tgz && rm -f tmp.tgz
+
+	fi
+	echo "none"
 done
